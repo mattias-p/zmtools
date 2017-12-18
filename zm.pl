c69076cbcd6651f3c4afdf36a3fe1bba2e82c92f
Mattias Päivärinta <mattias.paivarinta@iis.se>
2017-12-18 21:48:01 +0100

    Add start_domain_test, get_test_history and --verbose

--- zm.pl
+++ zm.pl
@@ -11,8 +11,9 @@ zm2 --help|--man
 zm2 command [command_options]
 
  Options:
-   --help  Brief help message
-   --man   Full documentation
+   --help     Brief help message
+   --man      Full documentation
+   --verbose  Show query
 
 =cut
 
@@ -33,39 +34,116 @@ sub main {
 
     my $opt_help;
     my $opt_man;
-
+    my $opt_verbose;
     GetOptionsFromArray(
         \@myopts,
         'help' => \$opt_help,
         'man' => \$opt_man,
+        'verbose' => \$opt_verbose,
     ) or pod2usage(2);
-
     pod2usage(1) if $opt_help;
     pod2usage(-verbose => 2) if $opt_man;
     pod2usage(1) if !@myopts;
 
     my $cmd = shift @myopts;
     my $cmd_sub = \&{ "cmd_" . $cmd };
-
     pod2usage("'$cmd' is not a command") if !defined &$cmd_sub;
 
-    &$cmd_sub(@myopts);
+    my $json = &$cmd_sub(@myopts);
+
+    if ($json) {
+        say $json if $opt_verbose;
+        my $request = to_request($json);
+        my $response = submit($request);
+        say $response;
+    }
 }
 
 sub cmd_version_info {
-    my $json = to_jsonrpc(
+    return to_jsonrpc(
         id => 1,
         method => 'version_info',
     );
-    my $req = to_post($json);
-    my $response = submit($req);
-    say $response;
+}
+
+sub cmd_start_domain_test {
+    my @opts = @_;
+
+    my %opt_nameservers;
+    my $opt_domain;
+    GetOptionsFromArray(
+        \@opts,
+        'domain|d=s' => \$opt_domain,
+        'nameserver|n=s' => \%opt_nameservers,
+    ) or pod2usage(2);
+
+    my %params = (
+        domain => $opt_domain,
+    );
+
+    if (%opt_nameservers) {
+        my @nameservers;
+        for my $name ( keys %opt_nameservers ) {
+            push @nameservers, {
+                ns => $name,
+                ip => $opt_nameservers{$name},
+            };
+        }
+        $params{nameservers} = \@nameservers;
+    }
+
+    return to_jsonrpc(
+        id => 1,
+        method => 'start_domain_test',
+        params => \%params,
+    );
+}
+
+sub cmd_get_test_history {
+    my @opts = @_;
+    my $opt_nameservers;
+    my $opt_domain;
+    my $opt_offset;
+    my $opt_limit;
+
+    GetOptionsFromArray(
+        \@opts,
+        'domain|d=s' => \$opt_domain,
+        'nameserver|n=s' => \$opt_nameservers,
+        'offset|o=i' => \$opt_offset,
+        'limit|l=i' => \$opt_limit,
+    ) or pod2usage(2);
+
+    my %params = (
+        frontend_params => {
+            domain => $opt_domain,
+        },
+    );
+
+    if ( $opt_nameservers ) {
+        $params{frontend_params}{domain} = json_tern($opt_nameservers);
+    }
+
+    if ( defined $opt_offset ) {
+        $params{offset} = $opt_offset;
+    }
+
+    if ( defined $opt_limit ) {
+        $params{limit} = $opt_limit;
+    }
+
+    return to_jsonrpc(
+        id => 1,
+        method => 'get_test_history',
+        params => \%params,
+    );
 }
 
 sub cmd_list {
     for my $name ( get_commands() ) {
         say $name;
     }
+    return;
 }
 
 sub get_commands {
@@ -74,6 +152,19 @@ sub get_commands {
     return
       map { $_ =~ s/^cmd_//r }
       grep { $_ =~ /^cmd_/ } grep { defined &{"main\::$_"} } keys %{"main\::"};
+}
+
+sub json_tern {
+    my $value = shift;
+    if ( $value eq 'true' ) {
+        return JSON::PP::true;
+    } elsif ( $value eq 'false' ) {
+        return JSON::PP::false;
+    } elsif ( $value eq 'null' ) {
+        return undef;
+    } else {
+        die "unknown ternary value";
+    }
 }
 
 sub to_jsonrpc {
@@ -92,7 +183,7 @@ sub to_jsonrpc {
     return encode_json($request);
 }
 
-sub to_post {
+sub to_request {
     my $json = shift;
 
     my $req = HTTP::Request->new(POST => 'http://localhost:5000/');
